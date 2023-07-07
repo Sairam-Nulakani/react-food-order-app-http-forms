3562c983b33066ae341974044ab68b456f6566d8
“Sairam-Nulakani” <sairamnulakani4@gmail.com>
2023-07-07 16:47:39 +0530

    fetching food items through http by db

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,55 @@
+import Card from "../UI/Card";
+import MealItem from "./MealItem/MealItem";
+import classes from "./AvailableMeals.module.css";
+import { useEffect, useState } from "react";
+
+const AvailableMeals = () => {
+  const [meals, setMeals] = useState([]);
+  useEffect(() => {
+    const fetchMeals = async () => {
+      try {
+        const response = await fetch(
+          "https://test-api-91566-default-rtdb.firebaseio.com/Meals.json"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch meals.");
+        }
+        const responseData = await response.json();
+        const loadedMeals = [];
+        for (const key in responseData) {
+          loadedMeals.push({
+            id: key,
+            name: responseData[key].name,
+            description: responseData[key].description,
+            price: responseData[key].price,
+          });
+        }
+        console.log(loadedMeals);
+        setMeals(loadedMeals);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchMeals();
+  }, []);
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
